20ae4944d9ce7499ac8bfb5a20d8bb7ce60f368d
levc4 <levc4kvlad@gmail.com>
2024-02-23 13:48:07 +0200

    Added more parameters verifying, make some test parametrized

--- core/src/test/java/greencity/controller/HabitControllerTest.java
+++ core/src/test/java/greencity/controller/HabitControllerTest.java
@@ -1,14 +1,22 @@
 package greencity.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import greencity.dto.PageableDto;
 import greencity.dto.habit.AddCustomHabitDtoRequest;
+import greencity.dto.habit.AddCustomHabitDtoResponse;
 import greencity.dto.habit.HabitDto;
+import greencity.dto.shoppinglistitem.ShoppingListItemDto;
+import greencity.dto.user.UserProfilePictureDto;
 import greencity.dto.user.UserVO;
 import greencity.service.HabitService;
 import greencity.service.TagsService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -18,17 +26,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.security.Principal;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.eq;
@@ -43,6 +46,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(MockitoExtension.class)
 public class HabitControllerTest {
     private static final String habitLink = "/habit";
+    private static final XmlMapper xmlMapper = new XmlMapper();
     private MockMvc mockMvc;
     @InjectMocks
     private HabitController habitController;
@@ -53,83 +57,88 @@ public class HabitControllerTest {
 
     @BeforeEach
     public void setUp() {
-        this.mockMvc = MockMvcBuilders
-                .standaloneSetup(habitController)
-                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
-                .build();
+        this.mockMvc = MockMvcBuilders.standaloneSetup(habitController).setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver()).build();
     }
 
-    //@TODO Ask about mockMvcTest and about returning HabitDto which dont have toString annotation
+    @ParameterizedTest
+    @CsvSource({"1", "5"})
+    void given_validHabitIdAndLocale_when_getHabitById_then_returnHabitDto(Long id) throws Exception {
+        HabitDto mockHabitDto = HabitDto.builder().id(1L).build();
 
-    @Test
-    void given_validHabitIdAndLocale_when_getHabitById_then_returnHabitDto() throws Exception {
-        HabitDto mockHabitDto = HabitDto.builder()
-                .id(1L)
-                .image("png")
-                .build();
+        when(habitService.getByIdAndLanguageCode(id, "en")).thenReturn(mockHabitDto);
 
-        when(habitService.getByIdAndLanguageCode(1L, "en")).thenReturn(mockHabitDto);
+        mockMvc.perform(get(STR."\{habitLink}/{id}", id)).andExpect(status().isOk()).andExpect(content().xml(xmlMapper.writeValueAsString(mockHabitDto)));
 
-        mockMvc.perform(get(STR."\{habitLink}/{id}", 1))
-                .andExpect(status().isOk())
-                .andExpect(content().json(TestResources.getFileContent("controller/habitController/expected-habit-dto-list.json")));
-
-        verify(habitService).getByIdAndLanguageCode(1L, "en");
+        verify(habitService).getByIdAndLanguageCode(id, "en");
     }
 
-    @Test
-    void given_userVOAndLocaleAndPageable_when_getAll_then_returnPageableDto() throws Exception {
-        int pageNumber = 1;
-        int pageSize = 2;
+    @ParameterizedTest
+    @CsvSource({"1, 2", "5, 10"})
+    void given_userVOAndLocaleAndPageable_when_getAll_then_returnPageableDto(Integer pageNumber, Integer pageSize) throws Exception {
         Pageable pageable = PageRequest.of(pageNumber, pageSize);
         UserVO userVO = new UserVO();
+        PageableDto<HabitDto> pageableDto = new PageableDto<>(Collections.emptyList(), 0, 0, 0);
 
-        mockMvc.perform(get(STR."\{habitLink}?page=1&size=2"))
-                .andExpect(status().isOk());
+        when(habitService.getAllHabitsByLanguageCode(userVO, pageable, "en")).thenReturn(pageableDto);
+
+        mockMvc.perform(get(STR."\{habitLink}").param("page", pageNumber.toString()).param("size", pageSize.toString())).andExpect(status().isOk()).andExpect(content().xml(xmlMapper.writeValueAsString(pageableDto)));
 
         verify(habitService).getAllHabitsByLanguageCode(userVO, pageable, "en");
     }
 
-    @Test
-    void given_validHabitIdAndLocale_when_getShoppingListItems_then_returnShoppingListItems() throws Exception {
-        mockMvc.perform(get(STR."\{habitLink}/{id}/shopping-list", 1))
-                .andExpect(status().isOk());
+    @ParameterizedTest
+    @CsvSource({"1", "5"})
+    void given_validHabitIdAndLocale_when_getShoppingListItems_then_returnShoppingListItems(Long id) throws Exception {
+        List<ShoppingListItemDto> shoppingListItemDtos = Collections.singletonList(new ShoppingListItemDto(2L, "test", "test"));
+        String expectedXmlString = "<List><item><id>2</id><text>test</text><status>test</status></item></List>";
 
-        verify(habitService).getShoppingListForHabit(1L, "en");
+        when(habitService.getShoppingListForHabit(id, "en")).thenReturn(shoppingListItemDtos);
+
+        MvcResult mvcResult = mockMvc.perform(get(STR."\{habitLink}/{id}/shopping-list", id).param("locale", "en")).andExpect(status().isOk()).andReturn();
+        String xmlContent = mvcResult.getResponse().getContentAsString();
+
+        Assertions.assertEquals(expectedXmlString, xmlContent);
+
+        verify(habitService).getShoppingListForHabit(id, "en");
     }
 
-    @Test
-    void given_tagsAndLocaleAndPageable_when_getAllByTagsAndLanguageCode_then_returnPageableDto() throws Exception {
-        int pageNumber = 5;
-        int pageSize = 20;
+    @ParameterizedTest
+    @CsvSource({"1, 2, eco", "5, 10, artificial"})
+    void given_tagsAndLocaleAndPageable_when_getAllByTagsAndLanguageCode_then_returnPageableDto(Integer pageNumber, Integer pageSize, String tag) throws Exception {
         Pageable pageable = PageRequest.of(pageNumber, pageSize);
-        List<String> tags = Collections.singletonList("eco");
+        List<String> tags = Collections.singletonList(tag);
+        PageableDto<HabitDto> pageableDto = new PageableDto<>(Collections.emptyList(), 0, 0, 0);
 
-        mockMvc.perform(get(STR."\{habitLink}/tags/search?tags=eco&page=5"))
-                .andExpect(status().isOk());
+        when(habitService.getAllByTagsAndLanguageCode(pageable, tags, "en")).thenReturn(pageableDto);
+
+        mockMvc.perform(get(STR."\{habitLink}/tags/search").param("tags", tag).param("page", pageNumber.toString()).param("size", pageSize.toString())).andExpect(status().isOk()).andExpect(content().xml(xmlMapper.writeValueAsString(pageableDto)));
 
         verify(habitService).getAllByTagsAndLanguageCode(pageable, tags, "en");
     }
 
-    @Test
-    void given_userVOAndLocaleAndTagsAndCustomHabitAndComplexitiesAndPageable_when_getAllByDifferentParameters_then_returnPageableDto() throws Exception {
-        int pageNumber = 5;
-        int pageSize = 20;
+    @ParameterizedTest
+    @CsvSource({"1, 2, eco, 7, false", "5, 10, artificial, 5, true"})
+    void given_userVOAndLocaleAndTagsAndCustomHabitAndComplexitiesAndPageable_when_getAllByDifferentParameters_then_returnPageableDto(Integer pageNumber, Integer pageSize, String tag, Integer complexity, Boolean isCustomHabit) throws Exception {
         Pageable pageable = PageRequest.of(pageNumber, pageSize);
         UserVO userVO = new UserVO();
-        Optional<List<String>> tags = Optional.of(Collections.singletonList("eco"));
+        Optional<List<String>> tags = Optional.of(Collections.singletonList(tag));
+        PageableDto<HabitDto> pageableDto = new PageableDto<>(Collections.emptyList(), 0, 0, 0);
 
-        mockMvc.perform(get(STR."\{habitLink}/search?tags=eco&isCustomHabit=false&complexities=7&page=5"))
-                .andExpect(status().isOk());
+        when(habitService.getAllByDifferentParameters(userVO, pageable, tags, Optional.of(isCustomHabit), Optional.of(Collections.singletonList(complexity)), "en")).thenReturn(pageableDto);
 
-        verify(habitService).getAllByDifferentParameters(userVO, pageable, tags,
-                Optional.of(false), Optional.of(Collections.singletonList(7)), "en");
+        mockMvc.perform(get(STR."\{habitLink}/search").param("tags", tag).param("isCustomHabit", isCustomHabit.toString()).param("complexities", complexity.toString()).param("page", pageNumber.toString()).param("size", pageSize.toString())).andExpect(status().isOk()).andExpect(content().xml(xmlMapper.writeValueAsString(pageableDto)));
+
+        verify(habitService).getAllByDifferentParameters(userVO, pageable, tags, Optional.of(isCustomHabit), Optional.of(Collections.singletonList(complexity)), "en");
     }
 
     @Test
     void given_validLocale_when_findAllHabitsTags_then_returnListOfTags() throws Exception {
-        mockMvc.perform(get(STR."\{habitLink}/tags?locale=en"))
-                .andExpect(status().isOk());
+        List<String> habits = Collections.singletonList("test");
+        String expectedXmlString = "<List><item>test</item></List>";
+
+        when(tagsService.findAllHabitsTags("en")).thenReturn(habits);
+
+        mockMvc.perform(get(STR."\{habitLink}/tags?locale=en")).andExpect(status().isOk()).andExpect(content().xml(expectedXmlString));
 
         verify(tagsService).findAllHabitsTags("en");
     }
@@ -137,44 +146,43 @@ public class HabitControllerTest {
     @Test
     void given_validRequestAndImageAndPrincipal_when_addCustomHabit_then_returnAddCustomHabitDtoResponse() throws Exception {
         Principal principal = Mockito.mock(Principal.class);
-        when(principal.getName()).thenReturn("Olivia.Johnson@gmail.com");
         String json = """
                         {
-                        "complexity": 1,
+                         "complexity": 1,
                          "defaultDuration": 1,
                          "habitTranslations": null,
-                        "customShoppingListItemDto": null,
+                         "customShoppingListItemDto": null,
                          "image": null,
                          "tagIds": null
-                         }
+                        }
                 """;
-        MockMultipartFile jsonFile =
-                new MockMultipartFile("request", "", "application/json", json.getBytes());
-
-        this.mockMvc.perform(multipart(STR."\{habitLink}/custom")
-                        .file(jsonFile)
-                        .principal(principal))
-                .andExpect(status().isCreated());
 
         ObjectMapper mapper = new ObjectMapper();
         AddCustomHabitDtoRequest addCustomHabitDtoRequest = mapper.readValue(json, AddCustomHabitDtoRequest.class);
+        AddCustomHabitDtoResponse addCustomHabitDtoResponse = mapper.readValue(json, AddCustomHabitDtoResponse.class);
 
-        verify(habitService)
-                .addCustomHabit(eq(addCustomHabitDtoRequest), isNull(), eq("Olivia.Johnson@gmail.com"));
+        MockMultipartFile jsonFile = new MockMultipartFile("request", "", "application/json", json.getBytes());
+
+        when(habitService.addCustomHabit(eq(addCustomHabitDtoRequest), isNull(), eq("Olivia.Johnson@gmail.com"))).thenReturn(addCustomHabitDtoResponse);
+        when(principal.getName()).thenReturn("Olivia.Johnson@gmail.com");
+
+        this.mockMvc.perform(multipart(STR."\{habitLink}/custom").file(jsonFile).principal(principal)).andExpect(status().isCreated()).andExpect(content().xml(xmlMapper.writeValueAsString(addCustomHabitDtoResponse)));
+
+        verify(habitService).addCustomHabit(eq(addCustomHabitDtoRequest), isNull(), eq("Olivia.Johnson@gmail.com"));
     }
 
-    @Test
-    void given_validHabitIdAndUserVO_when_getFriendsAssignedToHabitProfilePictures_then_returnListOfProfilePictures() throws Exception {
-        mockMvc.perform(get(STR."\{habitLink}/{habitId}/friends/profile-pictures", 1))
-                .andExpect(status().isOk());
+    @ParameterizedTest
+    @CsvSource({"1", "5"})
+    void given_validHabitIdAndUserVO_when_getFriendsAssignedToHabitProfilePictures_then_returnListOfProfilePictures(Long id) throws Exception {
+        List<UserProfilePictureDto> userProfilePictureDto = Collections.singletonList(new UserProfilePictureDto(id, "test", "test"));
 
-        verify(habitService).getFriendsAssignedToHabitProfilePictures(1L, null);
+        when(habitService.getFriendsAssignedToHabitProfilePictures(id, null)).thenReturn(userProfilePictureDto);
+
+        String expectedXmlString = STR."<List><item><id>\{id}</id><name>test</name><profilePicturePath>test</profilePicturePath></item></List>";
+
+        mockMvc.perform(get(STR."\{habitLink}/{habitId}/friends/profile-pictures", id)).andExpect(status().isOk()).andExpect(content().xml(expectedXmlString));
+
+        verify(habitService).getFriendsAssignedToHabitProfilePictures(id, null);
     }
 
-    static class TestResources {
-        static String getFileContent(String path) throws URISyntaxException, IOException {
-            URL resource = TestResources.class.getClassLoader().getResource(path);
-            return new String(Files.readAllBytes(Path.of(Objects.requireNonNull(resource).toURI())));
-        }
-    }
 }
